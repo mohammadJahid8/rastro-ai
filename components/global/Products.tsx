cf5f26531aa192c75a5e2f71e212dd77e2f7a94a
mohammadJahid8 <mohammadjahid0007@gmail.com>
2024-06-21 08:33:24 +0600

    products fetching optimized

--- components/global/Products.tsx
+++ components/global/Products.tsx
@@ -1,43 +1,16 @@
 /* eslint-disable @next/next/no-img-element */
 'use client';
-
-import axios from 'axios';
-
-import React, { useEffect, useState } from 'react';
 import { Button } from '../ui/button';
 import ExternalLink from '../icons/ExternalLink';
 import { useRouter } from 'next/navigation';
 import Link from 'next/link';
 
-const Products = () => {
-  const [products, setProducts] = useState([]);
-
+const Products = ({ products }: any) => {
   const router = useRouter();
-
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const response = await axios.get(
-          `https://sourcerer-production.up.railway.app/api/products?page_size=20`,
-          {
-            headers: {
-              Authorization: `Bearer eyJhbGciOiJSUzI1NiIsImtpZCI6ImRmOGIxNTFiY2Q5MGQ1YjMwMjBlNTNhMzYyZTRiMzA3NTYzMzdhNjEiLCJ0eXAiOiJKV1QifQ.eyJuYW1lIjoiTWQgSmFoaWQiLCJwaWN0dXJlIjoiaHR0cHM6Ly9saDMuZ29vZ2xldXNlcmNvbnRlbnQuY29tL2EvQUNnOG9jS2EtOGd4ZlB2M0Q5ZlNhLWtzTmtOSzlVeW9BUU8xenJvalhQS2xQWUhBQkQ3Z2M0VHE9czk2LWMiLCJpc3MiOiJodHRwczovL3NlY3VyZXRva2VuLmdvb2dsZS5jb20vcmFzdHJvLWFpIiwiYXVkIjoicmFzdHJvLWFpIiwiYXV0aF90aW1lIjoxNzE4ODE0NTA0LCJ1c2VyX2lkIjoiTUNkQlVyNWFTRlk3bGhjclN0b3ZLcTZMbEtIMyIsInN1YiI6Ik1DZEJVcjVhU0ZZN2xoY3JTdG92S3E2TGxLSDMiLCJpYXQiOjE3MTg5MzY2NDgsImV4cCI6MTcxODk0MDI0OCwiZW1haWwiOiJtb2hhbW1hZGphaGlkMDAwN0BnbWFpbC5jb20iLCJlbWFpbF92ZXJpZmllZCI6dHJ1ZSwiZmlyZWJhc2UiOnsiaWRlbnRpdGllcyI6eyJnb29nbGUuY29tIjpbIjExMTMxMzQ0MzQwNDE0NzE1NTExOCJdLCJlbWFpbCI6WyJtb2hhbW1hZGphaGlkMDAwN0BnbWFpbC5jb20iXX0sInNpZ25faW5fcHJvdmlkZXIiOiJnb29nbGUuY29tIn19.ABStM6MhIHmodyurtKvAUBbePI4sKp5cpPoZB32pg1TdBXpzZUYjlMjrIuK04U5IkK5Q5Pap1QCfmDadUtUVf8P09pD6QolfVl58-4izQ8eHCD3EAqGe8OhEYet0JN17JmcKPPbJQAY3KgS2aJh3s7Cd4FNl9muPnGOijeaR8uk9GrlHZO9juVHk_2EQSO9_wvPqUcIG3Vs5oBvFDe8UgGDbUZS4vz4k4UAYNoBTnzEKukZLHcHyV0c6A9_BkB3-NvKAZg-p2GUJSBYtxhDl0118E3eXcSXbSF8zskZG13mos0Y7GrTEnD--hGS-03CzqqhUvd5f8zfxLWklg0ehCA`,
-            },
-          }
-        );
-        console.log(response?.data);
-        setProducts(response?.data);
-      } catch (error) {
-        console.error('Error fetching liked products:', error);
-      }
-    };
-
-    fetchData();
-  }, []);
 
   return (
     <div className='columns-1 xs:columns-2 md:columns-3 lg:columns-5 2xl:columns-6 3xl:columns-7 gap-4 mt-10'>
-      {products?.map((item: any, index) => {
+      {products?.map((item: any, index: number) => {
         const closesAt: Date = new Date(item.closes_at);
         const now: Date = new Date();
         const isLessThan24: boolean =
