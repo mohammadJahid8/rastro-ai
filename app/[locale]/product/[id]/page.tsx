1fc30bcbcdb2b341c37475cb19c66fc093f8fd96
mohammadJahid8 <mohammadjahid0007@gmail.com>
2024-06-29 20:23:48 +0600

    latest

--- app/[locale]/product/[id]/page.tsx
+++ app/[locale]/product/[id]/page.tsx
@@ -1,17 +1,10 @@
 import { getProduct, getProducts, getSuggestions } from '@/actions/dataFetcher';
-import Navbar from '@/components/global/Navbar';
-import ProductCard from '@/components/global/ProductCard';
 import ProductDetails from '@/components/global/ProductDetails';
-import { SkeletonCard } from '@/components/global/SkeletonCard';
-import { ChevronLeft } from 'lucide-react';
-import dynamic from 'next/dynamic';
-import Link from 'next/link';
 
 type Props = {
   params: {
     id: string;
   };
-  
 };
 
 const page_size = 60;
@@ -19,13 +12,13 @@ const page_size = 60;
 const page = async ({ params }: Props) => {
   const product = await getProduct(params.id);
 
-  const suggestions = await getSuggestions(params.id, 1, page_size);
+  // const suggestions = await getSuggestions(params.id, 1, page_size);
 
   return (
     <div>
       <ProductDetails
         product={product}
-        suggestions={suggestions}
+        // suggestions={suggestions}
         productId={params.id}
       />
     </div>
--- components/global/ProductDetails.tsx
+++ components/global/ProductDetails.tsx
@@ -1,48 +1,62 @@
-"use client";
-import { ChevronLeft } from "lucide-react";
-import Link from "next/link";
-import ProductCard from "./ProductCard";
+'use client';
+import { ChevronLeft } from 'lucide-react';
+import Link from 'next/link';
+import ProductCard from './ProductCard';
 
-import Products from "./Products";
-import { useEffect, useState } from "react";
-import { useRouter } from "next/navigation";
+import Products from './Products';
+import { useEffect, useState } from 'react';
+import { useRouter } from 'next/navigation';
+import axiosInstance from '@/utils/axiosInstance';
 
-const ProductDetails = ({ product, suggestions, productId }: any) => {
+const ProductDetails = ({ product, productId }: any) => {
   const router = useRouter();
   const [searchQuery, setSearchQuery] = useState<string | null>(null);
+  const [suggestions, setSuggestions] = useState([]);
 
   useEffect(() => {
-    const query = sessionStorage.getItem("searchQuery");
+    const getSuggestions = async () => {
+      try {
+        const response = await axiosInstance.post(
+          `/product/${productId}/nearest?page=${1}&page_size=${10}`
+        );
+        setSuggestions(response.data);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    getSuggestions();
+
+    const query = sessionStorage.getItem('searchQuery');
     if (query) {
       setSearchQuery(query);
     }
-  }, []);
+  }, [productId]);
 
   const handleBack = () => {
-    if (searchQuery && searchQuery !== "null") {
-      console.log("Navigating to:", `/?search=${searchQuery}`);
+    if (searchQuery && searchQuery !== 'null') {
+      console.log('Navigating to:', `/?search=${searchQuery}`);
       router.push(`/?search=${searchQuery}`);
     } else {
-      console.log("Navigating to home");
-      router.push("/");
+      console.log('Navigating to home');
+      router.push('/');
     }
   };
   return (
-    <div className="flex lg:flex-row flex-col gap-4 h-full lg:px-10">
-      <div className="lg:w-[45%] xl:w-[35%] 2xl:w-[30%]  ">
-        <div className="flex items-start gap-4 w-full">
-          <div className="w-12 hidden lg:block">
+    <div className='flex lg:flex-row flex-col gap-4 h-full lg:px-10'>
+      <div className='lg:w-[45%] xl:w-[35%] 2xl:w-[30%]  '>
+        <div className='flex items-start gap-4 w-full'>
+          <div className='w-12 hidden lg:block'>
             <button
               onClick={handleBack}
-              className="bg-white w-12 shadow-md h-9 rounded-[8px] p-1 flex justify-center items-center group"
+              className='bg-white w-12 shadow-md h-9 rounded-[8px] p-1 flex justify-center items-center group'
             >
-              <ChevronLeft className="h-6 w-6 text-black " />
+              <ChevronLeft className='h-6 w-6 text-black ' />
             </button>
           </div>
           <ProductCard product={product} />
         </div>
       </div>
-      <div className="lg:w-[55%] xl:w-[65%] 2xl:w-[70%] px-5 lg:px-0">
+      <div className='lg:w-[55%] xl:w-[65%] 2xl:w-[70%] px-5 lg:px-0'>
         <Products
           initialProducts={suggestions}
           suggestionPage={true}
